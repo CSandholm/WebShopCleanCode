09cc2475211de04516569b3bb9e73d170624e95c
CSandholm <CharlesSandholm@gmail.com>
2023-04-05 12:00:15 +0200

    Added ProductProxy

---
+++ WebShopCleanCode/ProductProxy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopCleanCode
+{
+	internal class ProductProxy
+	{
+		public string Name { get; set; }
+		private Product product;
+		Database db;
+
+		public ProductProxy(string username, Database db)
+		{
+			Name = username;
+			this.db = db;
+		}
+	}
+}
